c98047cf4f108d1bf920844a71386367df356b66
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-01-09 09:11:44 +0200

    1/9/19

---
+++ main.js
@@ -0,0 +1,29 @@
+const squareOfSum = (a, b) => {
+	return(a**2+2*a*b+b**2);
+};
+
+squareOfSum(2, 3);
+squareOfSum(1, 10);
+
+
+// const identity = (value) => {
+// 	return value;
+// }
+
+// const identity = value => value;
+
+// const identity = function(value) {
+// 	return value;
+// }
+
+// function identity(value) {
+// 	return value;
+// }
+
+// const f = x => x * x;
+
+// const y = 5;
+// console.log(f(y));
+
+// const z = 3;
+// console.log(f(z));
--- solution_4.js
+++ solution_4.js
@@ -1,13 +1,25 @@
-/* eslint-disable no-console */
-
-const pi = 3.14; // eslint-disable-line
-
 // BEGIN (write your solution here)
-const weight = 67;
-const c = 300000000
-const energy = weight * c**2
+//a² + 2 * a * b + b²
+//2**2 + 2 * 2 * 3 + 3**2
+//4 + 12 + 9 = 25
 
+//const value(a, b) = > {
+//
+//}
 // END
 
-export default energy;
-console.info(`Your body energy is ${energy} joules. This is 1 / ${Math.round(389000000000000000000 / energy)} of the yearly world energy consumption. Woah!`);
+export default squareOfSum;
+
+///* eslint-disable no-console */
+//
+//const pi = 3.14; // eslint-disable-line
+//
+//// BEGIN (write your solution here)
+//const weight = 67;
+//const c = 300000000
+//const energy = weight * c**2
+//
+//// END
+//
+//export default energy;
+//console.info(`Your body energy is ${energy} joules. This is 1 / ${Math.round(389000000000000000000 / energy)} of the yearly world energy consumption. Woah!`);
