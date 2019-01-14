ad3cd622ed06d9eee73e6017016e4cbcd4fee3dc
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-01-14 17:24:03 +0200

    1/14/19

---
+++ algorithms.py
@@ -0,0 +1,30 @@
+"""insertion sort"""
+
+
+"""bubble sort"""
+# li = [5, 2, 7, 4, 0, 9, 8, 6]
+# li = [11, 2, 3, 444]
+# n = 1
+# while n < len(li):
+#     for i in range(len(li)-n):
+#         if li[i] > li[i+1]:
+#             li[i], li[i+1] = li[i+1], li[i]
+#     n += 1
+#
+# e = li
+# print(e)
+
+"""Bogosort"""
+# def bogosort(a):
+#     while not isSorted(a):
+#         shuffle(a)
+#
+#
+# bogosort(100)
+
+"""mult_by_add"""
+# def mult_by_add(a, b):
+#     return a * b
+#
+#
+# print(mult_by_add(7, 3))
---
+++ main.py
@@ -0,0 +1,8 @@
+# """mult_by_add"""
+#
+#
+# def mult_by_add(a, b):
+#     return a * b
+#
+#
+# print(mult_by_add(7, 3))
